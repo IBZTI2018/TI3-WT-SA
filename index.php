a1d3a80ef1befb989d75aa839703742791727a85
Sven Gehring <cbrxde@gmail.com>
2019-12-03 15:10:28 +0100

    set up docker, composer and basic php project

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-  phpinfo();
+  
+  // Use same namespace as sources autoloaded from `./src`.
+  // This way, we don't need to prefix all invocations!
+  namespace WTSA1;
+  
+  // Autoload source and dependencies via composer
+  require_once __DIR__ . '/vendor/autoload.php';
+
+  // Hello world!
+  print((new Greeter())->greet());
 ?>
---
+++ src/Greeter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WTSA1;
+
+class Greeter {
+  public function greet() {
+    print("Hello World!");
+  }
+}
+
+?>
