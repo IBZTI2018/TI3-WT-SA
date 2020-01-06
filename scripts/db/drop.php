2ab273434167b3a51aa054f28b01f81bec418611
Sven Gehring <cbrxde@gmail.com>
2020-01-06 07:27:56 +0100

    restructure database scripts

---
+++ scripts/db/drop.php
@@ -0,0 +1,28 @@
+<?php 
+
+/**
+ * Drop the database for  the diary software
+ * 
+ * This will drop all tables and constraints.
+ */
+
+use WTSA1\Engines\Database;
+use WTSA1\Engines\Hasher\PBKDF2;
+
+// Autoload source and dependencies via composer
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+function databaseDrop() {
+  
+  echo "Dropping `user` table...\r\n";
+  Database::getInstance()->query("
+    DROP TABLE IF EXISTS `user`
+  ");
+
+  echo "Done!\r\n";
+}
+
+// Automatically run script if executed via runscript
+if (getenv('AUTORUN_SCRIPT')) databaseDrop();
+
+?>
