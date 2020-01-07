76e01dcfb94e2fe9b082f9784b28dfd2b8911a9e
Sven Gehring <cbrxde@gmail.com>
2020-01-07 14:53:47 +0100

    hotfix: fix templates with non existent array key access

--- index.php
+++ index.php
@@ -6,9 +6,14 @@
   define('APP_DIR', __DIR__);
   define('TEMPLATE_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Templates');
 
-  // Global helper function for templating
+  // Global helper function for templating a variable
   function t($variable) {
     return (isset($variable)) ? $variable : "";
+  } 
+  
+  // Global helper function for templating an array key
+  function ta($array, $key) {
+    return (array_key_exists($key, $array)) ? $array[$key] : "";
   }
 
   // Start application
