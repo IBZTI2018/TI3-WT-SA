2ef748f5a0d968acace8ac73c8a5ac012f70cb01
Sven Gehring <cbrxde@gmail.com>
2020-01-07 09:06:40 +0100

    make db seeding demo-ready; #10

--- .webtechdeploy/postinstall.sh
+++ .webtechdeploy/postinstall.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Install dependencies
 composer install --no-dev
+
+# Recreate demo database
+php ./scripts/db/drop.php
+php ./scripts/db/create.php
+php ./scripts/db/seed.php
+
+# Delete unused files
+rm -rf ./scripts
 rm -rf ./tests
 rm -rf ./docs
