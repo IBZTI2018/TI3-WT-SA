5b40c37dd45233cf770e9ca5311a719e31c19c07
André Glatzl <andreglatzl@gmail.com>
2020-01-29 23:01:41 +0100

    Adding filter option for categories and date range

--- src/Pages/HomePage.php
+++ src/Pages/HomePage.php
@@ -9,7 +9,24 @@ class HomePage extends Page {
     protected $template = 'Home.phtml';
 
     protected function get() {
-        $this->data['entries'] = Entry::getEntriesForCurrentUser();
+        $filter_by = array();
+        if (isset($_REQUEST['filter_by_category_id']) && empty($_REQUEST['filter_by_category_id']) == false) {
+            $filter_by['category_id'] = intval($_REQUEST['filter_by_category_id']);
+        }
+        if (isset($_REQUEST['filter_by_from_date']) && empty($_REQUEST['filter_by_from_date']) == false) {
+            if (isset($filter_by['publish_date']) == false) {
+                $filter_by['publish_date'] = array();    
+            }
+            $filter_by['publish_date'][0] = $_REQUEST['filter_by_from_date'];
+        }
+
+        if (isset($_REQUEST['filter_by_until_date']) && empty($_REQUEST['filter_by_until_date']) == false) {
+            if (isset($filter_by['publish_date']) == false) {
+                $filter_by['publish_date'] = array();    
+            }
+            $filter_by['publish_date'][1] = $_REQUEST['filter_by_until_date'];
+        }
+        $this->data['entries'] = Entry::getEntriesForCurrentUser($filter_by);
     }
 }
 
